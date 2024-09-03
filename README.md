a53691ab8ed9995064d21fca329c5c2528577ff5
Yinghao-Li <www6com2010@hotmail.com>
2024-09-03 16:48:46 -0400

    udpate homework

---
+++ README.md
@@ -0,0 +1,36 @@
+# Assignment 1: Masked Multi-Head Attention
+
+## Task
+In this assignment, your task is to implement the Masked Multi-Head Attention mechanism as outlined in the "[Attention is All You Need](https://arxiv.org/pdf/1706.03762)" paper.
+Specifically, you'll complete the forward function of the `CausalSelfAttention` class located in `assignment1.py`.
+This class represents the Masked Multi-Head Attention layer within the Transformer *decoder*, so it's crucial to correctly apply the masking to prevent information leakage from future tokens.
+Note that the input sequence `x` is assumed to have no padding tokens.
+
+What you need to do is complete the code within
+```python
+# --- TODO: start of your code ---
+
+# --- TODO: end of your code ---
+```
+so that the model can run properly.
+Changing the code outside the `TODO` block is not necessary.
+
+
+## Test your implementation
+
+When you have finished implementing the forward function, you can run the code with
+```python
+python assignment1.py
+```
+
+If you implementation is correct, you will see "The output is correct." printed out on your terminal.
+Otherwise, you will encounter assertion errors.
+
+
+## Submission
+
+You can directly submit the modified `assignment1.py` file to Canvas.
+
+## Reference
+
+With default hyper-parameters, each training epoch takes roughly 30s to run on Mac M1 CPU, ~~18s on mps,~~ and 2s on Nvidia A5000/RTX4090 GPU.
---
+++ assignment1.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+
+d_model = 64
+n_head = 8
+dropout = 0.1
+
+batch_size = 2
+seq_len = 16
+
+
+class CausalSelfAttention(nn.Module):
+    """
+    A vanilla multi-head masked self-attention layer with a projection at the end.
+    """
+
+    def __init__(self, d_model=64, n_head=8, dropout=0.1):
+        super().__init__()
+        assert d_model % n_head == 0
+        self.q_proj = nn.Linear(d_model, d_model)
+        self.k_proj = nn.Linear(d_model, d_model)
+        self.v_proj = nn.Linear(d_model, d_model)
+        self.o_proj = nn.Linear(d_model, d_model)
+        self.residual_dropout = nn.Dropout(dropout)
+
+        self.n_head = n_head
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Implement the multi-head masked self-attention layer.
+        You should not use network modules other than what defined in the __init__ function.
+
+        Input & output shape: (batch_size, sequence_length, d_model)
+        """
+        # --- TODO: start of your code ---
+
+        # --- TODO: end of your code ---
+        raise NotImplementedError
+
+
+# causal_self_attention = CausalSelfAttention(d_model=d_model, n_head=n_head, dropout=dropout)
+
+# Load the model
+causal_self_attention = CausalSelfAttention(d_model=d_model, n_head=n_head, dropout=dropout)
+causal_self_attention.load_state_dict(torch.load("causal_self_attention.pt"))
+
+
+# Test the model
+# shape: (batch_size, seq_len, d_model)
+x = torch.load("x.pt")
+# x = torch.rand(2, 16, d_model)
+
+y = causal_self_attention(x)
+y_expected = torch.load("y.pt")
+
+assert y.shape == y_expected.shape, f"Expected shape: {y_expected.shape}, but got: {y.shape}"
+assert torch.sum(y == y_expected) > 0.78 * batch_size * seq_len * d_model, "The output is incorrect."
+
+print("The output is correct.")
